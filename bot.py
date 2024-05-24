ad16dd744f27bafe95f35c28c05979a59b40a2c7
kunai <neni23@mail.ru>
2024-05-24 18:38:09 +0300

    second part (ver. 2), with refactoring

--- bot.py
+++ bot.py
@@ -1,13 +1,15 @@
 import logging
 import random
 
-from _token import token
+try:
+    import settings
+except ImportError:
+    exit('Do cp settings.py.default settings.py and set token!')
 
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
 
-group_id = 225880064
 
 log = logging.getLogger('bot')
 log.setLevel(logging.DEBUG)
@@ -20,13 +22,23 @@ def configure_logging():
     log.addHandler(stream_handler)
 
     file_handler = logging.FileHandler('bot.log')
-    file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%m-%d-%Y %H:%M:%S'))
+    file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s',
+                                                datefmt='%m-%d-%Y %H:%M:%S'))
     file_handler.setLevel(logging.DEBUG)
     log.addHandler(file_handler)
 
 
 class Bot:
+    """
+    Echo bot для vk.com.
+
+    Use python 3.11
+    """
     def __init__(self, group_id, token):
+        """
+        :param group_id: group id из группы vk.com.
+        :param token: секретный токен из той же группы vk.com.
+        """
         self.group_id = group_id
         self.token = token
         self.vk_api = vk_api.VkApi(token=token)
@@ -34,6 +46,7 @@ class Bot:
         self.get_api = self.vk_api.get_api()
 
     def run(self):
+        """Запуск бота."""
         for event in self.long_poller.listen():
             try:
                 self.on_event(event)
@@ -41,6 +54,11 @@ class Bot:
                 log.exception('Ошибка в обработке события')
 
     def on_event(self, event):
+        """
+        Отправляет сообщение назад, если это текст.
+        :param event: VKBotMessageEvent object
+        :return: None
+        """
         if event.type == VkBotEventType.MESSAGE_NEW:
             log.info('Отправка сообщения обратно отправителю')
             self.get_api.messages.send(
@@ -54,5 +72,5 @@ class Bot:
 
 if __name__ == '__main__':
     configure_logging()
-    bot = Bot(group_id=group_id, token=token)
+    bot = Bot(group_id=settings.GROUP_ID, token=settings.TOKEN)
     bot.run()
