382f11ceb1f63f143437d74c5392429ed7a6fe27
kunai <neni23@mail.ru>
2024-07-27 18:43:09 +0300

    fifth part (ver.5), create invitation with avatar

--- create_invitation.py
+++ create_invitation.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+from io import BytesIO
 
+import requests
 from PIL import Image, ImageDraw, ImageFont
 
 
@@ -11,6 +13,9 @@ FONT_COLOR = (0, 0, 0)
 
 NAME_OFFSET = (270, 298)
 EMAIL_OFFSET = (270, 350)
+
+AVATAR_SIZE = '100x100'
+AVATAR_OFFSET = (80, 290)
 
 
 def generate_invitation(name, email):
@@ -28,4 +33,13 @@ def generate_invitation(name, email):
     draw.text(NAME_OFFSET, name, font=font, fill=FONT_COLOR)
     draw.text(EMAIL_OFFSET, email, font=font, fill=FONT_COLOR)
 
+    response = requests.get(url=f'https://robohash.org/{email}?size={AVATAR_SIZE}')
+    avatar_like_file = BytesIO(response.content)
+    avatar = Image.open(avatar_like_file)
+
+    base.paste(avatar, AVATAR_OFFSET)
+
     base.show()
+
+
+# generate_invitation('kunai', 'Depp')
